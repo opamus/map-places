ec6a561bd408a8e7a8a509f46ba29a8971891cf0
Opamus <ollilehmusvuori@gmail.com>
2019-05-13 21:09:51 +0300

    Removed index.php

--- map.php
+++ map.php
@@ -3,6 +3,7 @@
   <head>
       <meta charset="UTF-8">
       <link rel="stylesheet" type="text/css" href="styles/styles.css">
+      <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
   </head>
   <body>
       <div class="btn-markers">
